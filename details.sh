5a28af8c63d5f2af91152dbcb84c8cc073c43c54
Ajin Saji <ajinsaji2004@gmail.com>
2025-09-16 15:55:55 +0530

    Create details.sh

---
+++ details.sh
@@ -0,0 +1,17 @@
+echo "Enter your name:"
+read name
+echo "Enter your age :"
+read age
+echo "Enter your department:"
+read dept
+echo "enter your class:"
+read class
+echo "enter your roll_no:"
+read roll_no
+
+echo "My name is $name "
+echo "My age is $age "
+echo "My department is $dept "
+echo "My class is $class "
+echo "My roll no is $roll_no "
+echo "My name is $name and I am $age year old,currently pursuing my degree in $dept and the division and roll number is $class ,$roll_no respectively "
